40ea1d391e93ddb0c7ea9b34e7b0d37c0a66aa7e
ninakeijzer <nina_keijzer@hotmail.com>
2020-03-06 17:14:53 +0000

    MVP is done

--- app.rb
+++ app.rb
@@ -39,13 +39,23 @@ end
 get "/events/:id/rsvps/new" do
     puts "params: #{params}"
 
-    @event = events_table.where(id: params[:id]).to_a[0]
+     @event = events_table.where(id: params[:id]).to_a[0]
     view "new_rsvp"
 end
 
 get "/events/:id/rsvps/create" do
     puts "params: #{params}"
 
+    # first find the event that rsvp'ing for
+    @event = events_table.where(id: params[:id]).to_a[0] 
+    # next we want to insert a row in the rsvps table with the rsvp form data
+    rsvps_table.insert(
+        event_id: @event[:id],
+        name: params["name"],
+        email: params["email"],
+        comments: params["comments"],
+        going: params["going"]
+    ) 
     view "create_rsvp"
 end
 
